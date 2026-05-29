964838b3032d9ef7a8a23a25ae0aa3d2a036f0f8
SujiPrasanth <sujiprasantharumugam@gmail.com>
2026-05-29 11:43:21 +0530

    update proxy

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ app.use(cors(
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
+app.set("trust proxy", 1);
 app.use(session(
     {
         secret: process.env.SESSION_SECRET,
